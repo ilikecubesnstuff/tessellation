1b8e95b0d52ed01192046e5b057df4169fb6c4dd
ilikecubesnstuff <subho1618@gmail.com>
2023-08-30 13:58:53 +0100

    Setting up pre-commit hooks and automated formatting.

--- src/tessellation/__init__.py
+++ src/tessellation/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
-from typing import Any, Optional
 
 import inspect
+from importlib.metadata import version
+from typing import Any, Optional
 
 import numpy as np
 
@@ -10,45 +11,50 @@ from .dim2 import Tessellation2D as _Tess2D
 from .dim3 import Tessellation3D as _Tess3D
 from .generic import TessellationGeneric as _TessND
 
+__version__ = version("tessellation")
+
 
 class LowDimensionalityException(Exception):
     pass
 
 
-def Tessellation(orbit_or_point_array: Any,
-                 dims_for_orbit: Optional[tuple[str]] = None,
-                 *,
-                 incremental: bool = True,
-                 qhull_options: Optional[str] = None,
-                 axis_ratio: float = 10,
-                 normalization_routine: str = 'default',
-                 verbosity: int = 0,
-                 ) -> _TessType:
+def Tessellation(
+    orbit_or_point_array: Any,
+    dims_for_orbit: Optional[tuple[str]] = None,
+    *,
+    incremental: bool = True,
+    qhull_options: Optional[str] = None,
+    axis_ratio: float = 10,
+    normalization_routine: str = "default",
+    verbosity: int = 0,
+) -> _TessType:
     points = orbit = orbit_or_point_array
 
     pkg = None
     module = inspect.getmodule(orbit)
     if module:
-        pkg, *_ = module.__name__.partition('.')
+        pkg, *_ = module.__name__.partition(".")
 
-    if pkg in ('galpy', 'gala', 'agama'):
+    if pkg in ("galpy", "gala", "agama"):
         orbit = orbit_or_point_array
         if dims_for_orbit is None:
-            raise TypeError('Orbit dimensions must be supplied if tessellating an orbit object')
-        if pkg == 'galpy':
+            raise TypeError("Orbit dimensions must be supplied if tessellating an orbit object")
+        if pkg == "galpy":
             axes = [getattr(orbit, dim) for dim in dims_for_orbit]
             points = np.array([ax(orbit.t) for ax in axes]).T
-        elif pkg == 'gala':
+        elif pkg == "gala":
             points = np.array([getattr(orbit, dim).value for dim in dims_for_orbit]).T
-        elif pkg == 'agama':
+        elif pkg == "agama":
             raise NotImplementedError()
     else:
         if dims_for_orbit is not None:
-            raise ValueError('Orbit dimensions must only be passed with an orbit object')
+            raise ValueError("Orbit dimensions must only be passed with an orbit object")
         points = np.array(points, dtype=float)
 
     if points.ndim != 2:
-        raise ValueError('Tessellation can only be performed on a 2D array of shape (npoints, ndim)')
+        raise ValueError(
+            "Tessellation can only be performed on a 2D array of shape (npoints, ndim)"
+        )
 
     init_args = dict(
         points=points,
@@ -56,7 +62,7 @@ def Tessellation(orbit_or_point_array: Any,
         qhull_options=qhull_options,
         axis_ratio=axis_ratio,
         normalization_routine=normalization_routine,
-        verbosity=verbosity
+        verbosity=verbosity,
     )
 
     # NOTE: ndim is not the same as points.ndim
@@ -68,4 +74,6 @@ def Tessellation(orbit_or_point_array: Any,
     elif ndim > 3:
         return _TessND(**init_args)
     else:
-        raise LowDimensionalityException('Unsupported dimensionality, requires dimension 2 or greater')
+        raise LowDimensionalityException(
+            "Unsupported dimensionality, requires dimension 2 or greater"
+        )
